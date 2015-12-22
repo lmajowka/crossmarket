426f90055650b59ea081fdd349aebdcbce958c2f
Leo <leo.majowka@gmail.com>
2015-12-22 14:13:35 +0200

    fix #extract_location_from_user_header

--- app/models/dawanda_user.rb
+++ app/models/dawanda_user.rb
@@ -2,6 +2,8 @@ require 'dawanda'
 
 class DawandaUser < ActiveRecord::Base
 
+  LOCATION_DEFAULT_POSITION = 13
+
   validates_uniqueness_of :url
 
   searchkick
@@ -21,7 +23,15 @@ class DawandaUser < ActiveRecord::Base
   private
 
   def extract_location_from_user_header(user_header_inner)
-    user_header_inner.children[1].children[13].text.gsub(/\n|(Location)|\s/,'')
+    (0..LOCATION_DEFAULT_POSITION).each do |trial_index|
+      begin
+        text = user_header_inner.children[1].children[LOCATION_DEFAULT_POSITION - trial_index].text
+        if text.match(/Location/)
+          return text.gsub(/\n|(Location)|\s/,'')
+        end
+      rescue
+      end
+    end
   end
 
 end
